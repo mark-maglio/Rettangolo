9f7012a4b3454ef63d30fcb36cac450b7e0336f1
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-10-07 12:05:36 +0200

    Add project files.

---
+++ Rettangolo/Rettangolo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Rettangolo
+{
+    public class Rettangolo
+    {
+        private double x;
+        private double y;
+        private double widht;
+        private double height;
+
+        public Rettangolo(double x , double y, double w,double h)
+        {
+            this.x = x;
+            this.y = y;
+            this.widht = w;
+            this.height = h;
+        }
+        public double Area
+        {
+            get
+            {
+                return widht * height;
+            }
+        }
+        public double Perimetro
+        {
+            get
+            {
+                return widht * 2 + height * 2;
+            }
+        }
+        public bool Contiene(Rettangolo r)
+        {
+            if (x <= r.x && x + widht >= r.x + r.widht && y <= r.y && y + height >= r.y + r.height)
+                return true;
+            return false;
+        }
+    }
+}
